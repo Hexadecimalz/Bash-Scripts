aa8bca7b953f610bb8cb829feae39e4029a02e46
Hexadecimalz <Hexadecimalz@users.noreply.github.com>
2021-10-27 19:54:47 -0600

    add: simple script

---
+++ here.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# Show here document example
+
+#Cat Command Example 
+cat <<EndMSG
+---------------------------------
+This is line 1 of the message 
+This is line 2 of the message 
+This is the final line 
+---------------------------------
+EndMSG
+
+# Can also use here to create a list of command for a remote program that accepts text input
