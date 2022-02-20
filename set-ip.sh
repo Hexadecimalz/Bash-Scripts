7bac014eb6a8ab95dc593b5f3dd915d4f531f607
Hexadecimalz <Hexadecimalz@users.noreply.github.com>
2022-02-20 08:51:18 -0700

    debug: improve script logic

--- set-ip.sh
+++ set-ip.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-# Script to set a static IP on the default interface
-# For systems using nmcli
+# Script to set a static IP using nmcli
+## using a specified interface.
+## For systems using nmcli
 
 # Exit 4: missing 4 octets
-# Exit 5: No Nmcli found
+# Exit 5: no nmcli found
 # Exit 6: all other errors
+
+# Catch help calls, catch not sudo, catch no IP, catch no nic, catch no nmcli
+[[ "$1" =~ -*h+ ]] && show_help && exit 0
+[[ "$EUID" -ne 0 ]] && echo -e "Please run as sudo/root" && exit 6
+[[ -z "$1" ]] && echo -e "Please enter an IP"i && show_help && exit 4
+[[ -z "$2" ]] && echo -e "Please supply an interface" && show_help && exit 6
+[[ ! -f /usr/bin/nmcli ]] && echo -e "nmcli is not detected" && show_help && exit 5
+
 get_route(){
         route=$(ip route | grep "default" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
         echo -e "\t $route"
@@ -17,11 +26,17 @@ show_dns(){
 
 show_help(){
 cat <<HelpMSG
+==========================
 Usage: sudo bash set-ip.sh IP INTERFACE
+
+Prerequisites: 
+You must have nmcli installed
+to install: yum install NetworkManager
+==========================
 HelpMSG
 }
 
-#how to delete an IP
+# how to delete an IP
 # sudo ip addr del 192.168.1.69/32 dev eth0
 # Display some default information route/DNS
 echo -e "Type --help for help"
@@ -29,13 +44,6 @@ echo -e "Default Router:"
 get_route
 show_dns
 echo -e "***********************"
-
-# Catch help calls, catch not sudo, catch no IP, catch no nic, catch no nmcli
-[[ "$1" =~ -*h+ ]] && show_help && exit 0
-[[ "$EUID" -ne 0 ]] && echo -e "Please run as sudo/root" && exit 6
-[[ -z "$1" ]] && echo -e "Please enter an IP" && exit 4
-[[ -z "$2" ]] && echo -e "Please supply an interface" && exit 6
-[[ ! -f /usr/bin/nmcli ]] && echo -e "Nmcli is not detected" && exit 5
 
 # check that the IP provided is actually an IP
 if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
@@ -46,6 +54,6 @@ then
         nmcli | grep inet4
         sleep 3
 else
-        echo "IP didn't work try again"
+        echo "The IP you provided was not valid, please try again."
         echo 4
 fi
